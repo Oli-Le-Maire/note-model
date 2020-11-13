8869ccc821ca5da26a27ca68040ad1957cb16fc6
Oli Le Maire <olilemaire85@gmail.com>
2020-11-13 14:54:00 +0000

    added a function to change hello to howdy

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,5 +1,26 @@
-(function() {
-  let appDiv = document.getElementById('app');
-  let greeting = "howdy!"
-  appDiv.innerHTML = greeting;
-})(this);
+class NoteController {
+
+  constructor(noteListInstance = new NoteList) {
+    this.noteListInstance = noteListInstance;
+    this.noteListInstance.addNote("Favourite drink: seltzer");
+  }
+
+  helloToHowdy() {
+    let appDiv = document.getElementById('app');
+    let greeting = "howdy!";
+    appDiv.innerHTML = greeting;
+  };
+}
+
+var notecontroller = new NoteController
+notecontroller.helloToHowdy();
+
+
+//Code is in a file called note-controller.js.
+//Code uses Javascript classes to define a note controller that can be instantiated.
+// #The note controller constructor does the following:
+// #Takes a note list model as a parameter.
+// #Adds a note that says Favourite drink: seltzer.
+// #Creates a note list view, passing in the note list model.
+// #The note controller has a method that gets HTML from the
+// note list view and inserts it into the app element.
